6ca22774ffa4b7902910bc5285120954229abe38
Misty De Méo <misty.demeo@shopify.com>
2022-05-09 16:05:11 -0700

    babby's first UI

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,69 +1,21 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "syscall/js"
+	"syscall/js"
+
+	"github.com/arcadia-rose/shopify-hackdays-march-2022/internal/game"
 )
 
-type Link struct {
-  CharacterStartIndex uint   `json:"characterStartIndex"`
-  CharacterEndIndex   uint   `json:"characterEndIndex"`
-  Href                string `json:"href"`
-}
-
-type Page struct {
-  Content string `json:"content"`
-  Links   []Link `json:"links"`
-}
-
-func Example() js.Func {
-  example := Page {
-    Content: "Hello, Shopify!",
-    Links: []Link{
-      {
-        CharacterStartIndex: 0,
-        CharacterEndIndex: 14,
-        Href: "https://shopify.com",
-      },
-    },
-  }
-
-  return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-    encoded, _ := json.Marshal(example)
-    return string(encoded)
-  })
-}
-
-func render(page Page) string {
-  // Assume one link because multiple is complicated lol
-  link := page.Links[0]
-  pre := page.Content[:link.CharacterStartIndex]
-  post := page.Content[link.CharacterEndIndex:]
-  between := page.Content[link.CharacterStartIndex:link.CharacterEndIndex]
-
-  return fmt.Sprintf("%s<a href=\"%s\">%s</a>%s", pre, link.Href, between, post)
-}
-
-func Render() js.Func {
-  return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-    if len(args) != 1 {
-      return "Missing page argument"
-    }
-
-    page := Page{}
-
-    err := json.Unmarshal([]byte(args[0].String()), &page)
-    if err != nil {
-      return "Invalid page argument"
-    }
-
-    return render(page)
-  })
+func NewState() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		s := game.NewState()
+		return map[string]interface{}{
+			"PlayerState": s.PlayerState.ToMap(),
+		}
+	})
 }
 
 func main() {
-  js.Global().Set("Example", Example())
-  js.Global().Set("Render", Render())
-  <-make(chan bool)
+	js.Global().Set("NewState", NewState())
+	<-make(chan bool)
 }
--- internal/game/game.go
+++ internal/game/game.go
@@ -8,57 +8,66 @@ type Room struct {
 }
 
 type Item struct {
-  Name string
-  Amount uint
+	Name   string
+	Amount uint
 }
 
 type Player struct {
-  Insight uint
-  Position Id
-  Inventory map[Id]Item
+	Insight   uint
+	Position  Id
+	Inventory map[Id]Item
 }
 
 type State struct {
-  PlayerState Player
-  EventHandlers map[Event]func(State, []Id) State
+	PlayerState   Player
+	EventHandlers map[Event]func(State, []Id) State
 }
 
-var ItemRegistry = map[Id]Item {
-  Id(0): Item{
-    Name: "Key",
-    Amount: 1,
-  },
+var ItemRegistry = map[Id]Item{
+	Id(0): Item{
+		Name:   "Key",
+		Amount: 1,
+	},
 }
 
 func GameLoop(state State, event Event, ids []Id) State {
-  handler, found := state.EventHandlers[event]
-  if found {
-    return handler(state, ids)
-  }
-  return state
+	handler, found := state.EventHandlers[event]
+	if found {
+		return handler(state, ids)
+	}
+	return state
 }
 
 func NewPlayer() Player {
-  return Player {
-    Insight: 0,
-    Position: Id(0),
-    Inventory: map[Id]Item{},
-  }
+	return Player{
+		Insight:   0,
+		Position:  Id(0),
+		Inventory: map[Id]Item{},
+	}
+}
+
+func (p Player) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Insight":  p.Insight,
+		"Position": int(p.Position),
+		// When we figure out how we actually want to represent this
+		"Inventory": []interface{}{},
+	}
 }
 
 func NewState() State {
-  return State{
-    PlayerState: NewPlayer(),
-    EventHandlers: map[Event]func(State, []Id) State {
-      Event("collectItem"): collectItem,
-    },
-  }
+	return State{
+		PlayerState: NewPlayer(),
+		EventHandlers: map[Event]func(State, []Id) State{
+			Event("collectItem"): collectItem,
+		},
+	}
 }
 
 func collectItem(state State, itemIds []Id) State {
-  for _, itemId := range itemIds {
-    state.PlayerState.Inventory[itemId] = ItemRegistry[itemId]
-  }
+	for _, itemId := range itemIds {
+		state.PlayerState.Inventory[itemId] = ItemRegistry[itemId]
+	}
 
-  return state
+	return state
 }
