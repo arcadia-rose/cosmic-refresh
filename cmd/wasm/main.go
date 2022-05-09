748bf2ab9b8e95655acc52956a49ef90c496af60
Misty De Méo <misty.demeo@shopify.com>
2022-05-09 16:56:25 -0700

    Basic inventory increment between Go/JS

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -6,16 +6,33 @@ import (
 	"github.com/arcadia-rose/shopify-hackdays-march-2022/internal/game"
 )
 
+var State game.State
+
 func NewState() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		s := game.NewState()
-		return map[string]interface{}{
-			"PlayerState": s.PlayerState.ToMap(),
+		State = game.NewState()
+		return State.ToMap()
+	})
+}
+
+func GameLoop() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		event := args[0].String()
+		argc := args[1].Int()
+		ids := []game.Id{}
+		for i := 0; i < argc; i++ {
+			index := i + 2 // starting index is 2
+			ids = append(ids, game.Id(args[index].Int()))
 		}
+
+		State = game.GameLoop(State, game.Event(event), ids)
+
+		return State.ToMap()
 	})
 }
 
 func main() {
 	js.Global().Set("NewState", NewState())
+	js.Global().Set("GameLoop", GameLoop())
 	<-make(chan bool)
 }
