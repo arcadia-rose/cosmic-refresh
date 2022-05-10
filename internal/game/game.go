2d3dcdc9a43983832a6a0dece4659e99cb7351e5
Arcadia Rose <arcadia.rose@shopify.com>
2022-05-10 11:33:08 -0400

    Represent rooms as spaces with flavour text and generic actions that can be performed in them

--- internal/game/game.go
+++ internal/game/game.go
@@ -4,7 +4,29 @@ type Id uint
 
 type Event string
 
+type Entity uint
+
+const (
+  CollectItemEvt = Event("collectItem")
+  EnterRoomEvt   = Event("enterRoom")
+)
+
+const (
+  ItemE Entity = 1
+  RoomE Entity = iota
+)
+
+type Action struct {
+  Do Event  `json:"do"`
+  It string `json:"it"`
+  To Id     `json:"to"`
+  Is Entity `json:"is"`
+}
+
 type Room struct {
+  Description string   `json:"description"`
+  Items       []Item   `json:"items"`
+  Actions     []Action `json:"actions"`
 }
 
 type Item struct {
@@ -14,19 +36,20 @@ type Item struct {
 
 type Player struct {
   Insight   uint        `json:"insight"`
-  Position  Id          `json:"position"`
   Inventory map[Id]Item `json:"inventory"`
 }
 
 // Internal representation of the game state only usably on the Go side.
 type State struct {
 	PlayerState   Player
+  CurrentRoom   Room
 	EventHandlers map[Event]func(State, []Id) State
 }
 
 func (s State) View() StateView {
   return StateView {
     PlayerState: s.PlayerState,
+    CurrentRoom: s.CurrentRoom,
   }
 }
 
@@ -34,11 +57,13 @@ func (s State) View() StateView {
 // The non-serializable components of `State` must be reconstructed for internal use.
 type StateView struct {
   PlayerState Player `json:"player"`
+  CurrentRoom Room   `json:"currentRoom"`
 }
 
 func (s StateView) State() State {
   newState := NewState()
   newState.PlayerState = s.PlayerState
+  newState.CurrentRoom = s.CurrentRoom
   return newState
 }
 
@@ -47,6 +72,42 @@ var ItemRegistry = map[Id]Item{
 		Name:   "Key",
 		Amount: 1,
 	},
+}
+
+var RoomRegistry = map[Id]Room{
+  Id(1000): MainEntrance(),
+  Id(1001): ShoeRoom(),
+}
+
+func MainEntrance() Room {
+  description := `You approach a big spooky door!`
+
+  actions := []Action{
+    {
+      Do: EnterRoomEvt,
+      It: "Enter",
+      To: Id(1001),
+      Is: RoomE,
+    },
+  }
+
+  return Room {
+    Description: description,
+    Items: []Item{},
+    Actions: actions,
+  }
+}
+
+func ShoeRoom() Room {
+  description := `The entrance is surprisingly spaceous.  To your right is a large rack for
+  leaving one's shoes.  It's unlikely that a single living soul has been through here in some time
+  and the shoes left on the rack are tattered and falling to pieces.`
+
+  return Room {
+    Description: description,
+    Items: []Item{},
+    Actions: []Action{},
+  }
 }
 
 func GameLoop(state State, event Event, ids []Id) State {
@@ -60,7 +121,6 @@ func GameLoop(state State, event Event, ids []Id) State {
 func NewPlayer() Player {
 	return Player{
 		Insight:   0,
-		Position:  Id(0),
 		Inventory: map[Id]Item{},
 	}
 }
@@ -68,8 +128,10 @@ func NewPlayer() Player {
 func NewState() State {
 	return State{
 		PlayerState: NewPlayer(),
+    CurrentRoom: MainEntrance(),
 		EventHandlers: map[Event]func(State, []Id) State{
-			Event("collectItem"): collectItem,
+			CollectItemEvt: collectItem,
+      EnterRoomEvt: enterRoom,
 		},
 	}
 }
@@ -80,4 +142,13 @@ func collectItem(state State, itemIds []Id) State {
 	}
 
 	return state
+}
+
+func enterRoom(state State, roomIds []Id) State {
+  if len(roomIds) != 1 {
+    return state
+  }
+
+  state.CurrentRoom = RoomRegistry[roomIds[0]]
+  return state
 }
